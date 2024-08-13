6299c147f40998a0f03ea2adfa5b55a1cad385aa
Der Robert <erictainment@live.de>
2024-08-13 22:58:10 +0200

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace RVM.Sharp;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
